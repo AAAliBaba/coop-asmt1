22d3ac31af75e1b00eb4b646962e88a3ada207e2
Arafat Arman <ayyarman32o@gmail.com>
2019-01-26 19:49:48 -0500

    coop_asmt1 folder
    coop_asmt1 folder contains:
     - my favorite editor and why (fave_editor.txt)
     - shell script (txt_script.sh)
     - "textfiles" folder (contains .txt files)

---
+++ coop_asmt1/txt_script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "Week 1 Assignment Shell Script by Arafat Arman"
+
+directory="textfiles" #directory that contains all .txt files we want to change
+cd $directory
+
+#parsing through each .txt file in current directory
+for file in *.txt; do
+
+	line_count=$(<"$file" wc -l) #stores only file line count (using input redirection)
+	file_name=$(basename $file) #stores file name+extension
+
+echo "$file_name has $line_count lines"
+	#conditional checks
+	if [ $line_count -eq 0 ]; then
+		echo "Deleting ...\n"
+		rm $file
+	elif [ $line_count -lt 10 ]; then
+		echo "Renaming $file_name to short_$file_name ..."
+		mv $file_name "short_$file_name"
+	elif [ $line_count -ge 10 -a $line_count -le 20 ]; then
+		echo "Renaming $file_name to medium_$file_name ..."
+		mv $file_name "medium_$file_name"
+	else
+		echo "Renaming $file_name to long_$file_name ..."
+		mv $file_name "long_$file_name"
+	fi
+done
+
+echo "Script complete"
